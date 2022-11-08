f5376abe3e479b4b3ad254aec4e4fd83e4eac4c3
Mr.Burnham <sburnham3@gmail.com>
2022-11-07 21:05:31 -0500

    Add Intake system

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,12 @@ package frc.robot;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -18,8 +22,12 @@ import edu.wpi.first.wpilibj.TimedRobot;
 public class Robot extends TimedRobot {
 
   private CANSparkMax leftFront, leftRear, rightFront, rightRear;
+  private CANSparkMax intakeMotor;
 
-  private Joystick leftStick, rightStick;
+  private DoubleSolenoid intakeSolenoid;
+
+  private Joystick leftStick, rightStick, controller;
+  private JoystickButton aIntakeBall, bEjectBall, xIntakeExtend, yIntakeRetract; 
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -38,6 +46,12 @@ public class Robot extends TimedRobot {
     leftRear = new CANSparkMax(44, MotorType.kBrushless); //44 is the SparkMax ID# 
     rightFront = new CANSparkMax(43, MotorType.kBrushless); //43 is the SparkMax ID#
     rightRear = new CANSparkMax(45, MotorType.kBrushless); //45 is the SparkMax ID#
+
+    intakeMotor = new CANSparkMax(39, MotorType.kBrushless); //39 is the SparkMax ID#
+
+    //Instantiate the intake double solenoid,connected to solenoid channels 1 and 0 on the PCM
+    intakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 0);
+
  
     // Set right front controller to be inverted
     leftFront.setInverted(false);
@@ -46,7 +60,16 @@ public class Robot extends TimedRobot {
     // Set rear controllers to follow front
     leftRear.follow(leftFront);
     rightRear.follow(rightFront);
-  }
+
+    //Declare the controller buttons which will control the intake extend/retract solenoid
+    //and the controller buttons which will spin the Intake motor forward/reverse
+    //Find the button numbers using the tab on the Driver Station and pushing the buttons 
+    aIntakeBall = new JoystickButton(controller, 2); //2 is the A button on the controller
+    bEjectBall = new JoystickButton(controller, 3); // 3 is the B button on the controller
+    xIntakeExtend = new JoystickButton(controller, 1); //1 is the X button on the controller
+    yIntakeRetract = new JoystickButton(controller, 4); //4 is the Y button on the controller
+  
+}
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
    * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
@@ -75,6 +98,21 @@ public class Robot extends TimedRobot {
     //Set front drive motors speed according to the Y axes on the two joysticks (Tank Drive)
     leftFront.set(-leftStick.getY());
     rightFront.set(-rightStick.getY());
+
+    //Intake solenoid forward if A button (2) pressed on controller, reverse if B (3) pressed
+    if (aIntakeBall.get()) {
+      intakeSolenoid.set(Value.kForward);
+    } else if (bEjectBall.get()) {
+      intakeSolenoid.set(Value.kReverse);
+    }
+   
+  //Intake motor forward 1/2 speed if X (1) pressed on controller, reverse if Y (4) pressed
+    if (xIntakeExtend.get()) {
+      intakeMotor.set(0.5);
+    } else if (yIntakeRetract.get()) {
+      intakeMotor.set(-0.5);
+    }
+
   }
 
   /** This function is called once when the robot is disabled. */
